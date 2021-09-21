86774c2cc3effe0001d53f74bba712f0eb809637
George <georgesucuzhanay100@gmail.com>
2021-09-20 21:31:45 -0400

    lab work

--- string.js
+++ string.js
@@ -1,3 +1,7 @@
+// Group Members:
+// Melanie Vasquez and Khazinne Fofanah
+
+
 //DrEvil
 //create a function called DrEvil. It should take a single argument, an amount
 //and return '<amount> dollars', except it will add '(pinky)' at the end if 
@@ -6,11 +10,14 @@
 // DrEvil(1000000): 1000000 dollars (pinky)
 // answer below:
 
-
-
-
-
-
+function DrEvil (number) {
+    if (number >= 1000000) {
+      return number + " dollars" + "(pinky)"
+    }
+    else if (number < 1000000) {
+      return number + " dollars"
+    }
+  }
 
 //Create a function called mixUp
 //It should take in two stings, and return the concatenation of the two strings(separated by a space)
@@ -20,10 +27,9 @@
 //mixUp('dog', 'dinner'): 'dig donner'
 //write answer below 
 
-
-
-
-
+function mixUp (wordone, wordtwo) {
+    return wordtwo.slice(0, 2) + wordone.slice(2) + " " + wordone.slice(0, 2) + wordtwo.slice(2);
+  }
 
 //Create a function called fixStart
 //It should take a single argument, a string, and return a version where all occurences of its first
@@ -31,10 +37,19 @@
 //fixstart('babble'): 'ba**le'
 //write answer below
 
-
-
-
-
+function fixStart (word) {
+    let firstChar = word[0];
+    let result = word[0];
+    for (let i = 1;i < word.length; i++){
+        if (word[i] == firstChar){
+            result = result + "*";
+        }
+        else {
+            result = result + word[i];
+        }
+    }
+    return result;
+  }
 
 //Create a function called verbing. It should take a single argument, a string.
 //If it's length is at least 3, it should add 'ing' to its end, unless it already ends in 'ing',
@@ -45,12 +60,20 @@
 //verbing('go'): 'go'
 //write answer below
 
-
-
-
-
-
-
+function verbing (word) {
+    for (i = 0; i < word.length; i++){
+      
+      if (word.length < 3) {
+        return word
+      }
+      else if (word.slice(-3) == 'ing') {
+        return word + "ly"
+      }
+      else {
+        return word + "ing"
+      }
+    }
+  }
 
 //Create a function called notBad that takes a single argument, a string
 //It should find the first appearance of the substring 'not' and 'bad'
@@ -63,3 +86,22 @@
 //notBad('This movie is not so bad!'): 'This movie is good!'
 //notBad('This dinner is bad!'): 'This dinner is bad!'
 //write answer below 
+
+function notBad (sentence) {
+
+    for (i = 0; i < sentence.length; i++) {
+      if (sentence.includes("not") + sentence.includes("bad") {
+        newword = sentence.replace("not", "good")
+        return sentence
+      } 
+      else {
+        return sentence
+      }
+    }
+  }
+  
+  
+  
+  console.log(notBad('This dinner is not that bad!'))
+  console.log(notBad('This movie is not so bad!'))
+  console.log(notBad('This dinner is bad!'))
